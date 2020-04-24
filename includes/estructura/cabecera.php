a87613200d231f8d1f1609fc329ead91224ce56f
Vicky <Vicky>
2020-04-24 02:04:00 +0200

    Added: carroCompra

--- includes/estructura/cabecera.php
+++ includes/estructura/cabecera.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>HOME</title>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -48,7 +47,10 @@
                         ?>
                     </ul>
                 </li>
-            </ul>
+                <?php
+                    include __DIR__.'/carroCompra.php';
+                ?>
+             </ul>
         </nav>
     </body>
 </html>
