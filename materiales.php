3b02668e65146ec7d25ba16364e575aee849183f
SergyC <63809662+SergyC@users.noreply.github.com>
2020-04-28 12:41:41 +0200

    Update materiales.php

--- materiales.php
+++ materiales.php
@@ -16,58 +16,94 @@ require_once __DIR__.'/includes/config.php';
 				include __DIR__.'/includes/estructura/cabecera.php';
 		?>
 		<div id="contenido">
-			<div class="material1">
-					<img src="img/productos/esquies.jpg"width="500" height="250">
-					<table>
-					<td>
-						<div class="contenido">ESQUÌS</div>
-					</td>
-					<td>
-						<button><a href="">ACERCA DE</a></button>
-					</td>
-					<td>
-						<button><a href="">COMPRAR</a></button>
-					</td>
-					<td>
-						<button><a href="">ALQUILA</a></button>
-					</td>
-				</table>
-			</div>
-			<br></br>
-			<div class="material2">
-				<img src="img/productos/patines.jpg"width="500" height="250">
-				<table>
-					<td>
-						<div class="contenido">PATINES</div>
-					</td>
-					<td>
-						<button><a href="">ACERCA DE</a></button>
-					</td>
-					<td>
-						<button><a href="">COMPRAR</a></button>
-					</td>
-					<td>
-						<button><a href="">ALQUILA</a></button>
-					</td>
-				</table>
-			</div>
-			<br></br>
-			<div class="material3">
-				<img src="img/productos/bate.jpg"width="500" height="250">
-				<table>
-					<td>
-						<div class="contenido">BATES</div>
-					</td>
-					<td>
-						<button><a href="">ACERCA DE</a></button>
-					</td>
-					<td>
-						<button><a href="">COMPRAR</a></button>
-					</td>
-					<td>
-						<button><a href="">ALQUILA</a></button>
-					</td>
-				</table>
+			<div id="interior">
+				<div class="material1">
+						<?php 
+							$app = aplicacion::getSingleton();
+							$conexion = $app->conexionBd();
+							$tabla = "productos_disponibles";
+							if ($conexion->connect_error) {
+								die("La conexion falló: " . $conexion->connect_error);
+							}
+							else{
+								$query = "SELECT * FROM $tabla WHERE id = 8";
+								$result = $conexion->query($query);
+								$row = mysqli_fetch_assoc($result);
+								$_SESSION['nombre_producto']=$row['nombre'];
+								$_SESSION['imagen']=$row['imagen'];
+							}
+							$img=$_SESSION['imagen'];
+							echo "<img src=\"/cornersports/img/$img\" width=\"325\" height=\"300\">";
+						?>
+						<br>
+						<div class="entrenador">
+							<?php
+								$nombre=$_SESSION['nombre_producto'];
+								echo "$nombre";
+							?>
+							<button><a href="procesos/procesarMaterial.php?id=8">COMPRAR</a>
+							<button><a href="procesos/procesarMaterial.php?id=8">ALQUILA</a></button>
+						</div>
+			
+				</div>
+				<br></br>
+				<div class="material2">
+					<?php 
+							$app = aplicacion::getSingleton();
+							$conexion = $app->conexionBd();
+							$tabla = "productos_disponibles";
+							if ($conexion->connect_error) {
+								die("La conexion falló: " . $conexion->connect_error);
+							}
+							else{
+								$query = "SELECT * FROM $tabla WHERE id = 9";
+								$result = $conexion->query($query);
+								$row = mysqli_fetch_assoc($result);
+								$_SESSION['nombre_producto']=$row['nombre'];
+								$_SESSION['imagen']=$row['imagen'];
+							}
+							$img=$_SESSION['imagen'];
+							echo "<img src=\"/cornersports/img/$img\" width=\"325\" height=\"300\">";
+						?>
+						<br>
+						<div class="entrenador">
+							<?php
+								$nombre=$_SESSION['nombre_producto'];
+								echo "$nombre";
+							?>
+							<button><a href="procesos/procesarMaterial.php?id=9">COMPRAR</a>
+							<button><a href="procesos/procesarMaterial.php?id=9">ALQUILA</a></button>
+						</div>
+				</div>
+				<br></br>
+				<div class="material3">
+					<?php 
+						$app = aplicacion::getSingleton();
+						$conexion = $app->conexionBd();
+						$tabla = "productos_disponibles";
+						if ($conexion->connect_error) {
+							die("La conexion falló: " . $conexion->connect_error);
+						}
+						else{
+							$query = "SELECT * FROM $tabla WHERE id = 10";
+							$result = $conexion->query($query);
+							$row = mysqli_fetch_assoc($result);
+							$_SESSION['nombre_producto']=$row['nombre'];
+							$_SESSION['imagen']=$row['imagen'];
+						}
+						$img=$_SESSION['imagen'];
+						echo "<img src=\"/cornersports/img/$img\" width=\"325\" height=\"300\">";
+					?>
+					<br>
+					<div class="entrenador">
+						<?php
+							$nombre=$_SESSION['nombre_producto'];
+							echo "$nombre";
+						?>
+						<button><a href="procesos/procesarMaterial.php?id=10">COMPRAR</a>
+						<button><a href="procesos/procesarMaterial.php?id=10">ALQUILA</a></button>
+					</div>
+				</div>
 			</div>
 		</div>
 		<?php
