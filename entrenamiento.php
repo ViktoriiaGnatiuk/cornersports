4a04761c8c3995ca7e6eef2d792d83018199000d
SergyC <63809662+SergyC@users.noreply.github.com>
2020-04-29 10:24:46 +0200

    Update entrenamiento.php

--- entrenamiento.php
+++ entrenamiento.php
@@ -15,19 +15,83 @@ require_once __DIR__.'/includes/config.php';
         <div id="contenido">
             <div id="interior">
                 <div id="entreno_1">
-                    <img src="https://www.elnuevoherald.com/vivir-mejor/salud/wnhptb/picture85493327/alternates/FREE_1140/WQD00%20Women%20News%20rk" width="500" height="330">
-                    <div id="texto-encima">Entrenamiento personal en gimnasio</div>
-                    <div id="entrenador">Entrenador: Sergio Crespillo Campos
-                        <button><a href="procesos/procesarEntreno.php?id=1">CONTRATAR</a></button>
-                    </div>
+					<?php 
+                        $app = aplicacion::getSingleton();
+                        $conexion = $app->conexionBd();
+                        $tabla = "entrenamientos";
+                        if ($conexion->connect_error) {
+                            die("La conexion falló: " . $conexion->connect_error);
+                        }
+                        else{
+                            $query = "SELECT * FROM $tabla WHERE id = 20";
+                            $result = $conexion->query($query);
+                            $row = mysqli_fetch_assoc($result);
+                            $_SESSION['nombre']=$row['Nombre'];
+							$_SESSION['entrenador']=$row['Entrenador'];
+                        }
+					?>
+					<img src="https://www.elnuevoherald.com/vivir-mejor/salud/wnhptb/picture85493327/alternates/FREE_1140/WQD00%20Women%20News%20rk" width="500" height="330">
+					<div id="texto-encima">
+						<?php
+							$nombre=$_SESSION['nombre'];
+							echo "$nombre";
+						?>
+					</div>
+					<div id="entrenador">
+						<?php
+							$entrenador=$_SESSION['entrenador'];
+							
+							$tabla = "entrenadores";
+							$query = "SELECT * FROM $tabla WHERE id = $entrenador";
+							$result = $conexion->query($query);
+							$row = mysqli_fetch_assoc($result);
+							$_SESSION['nombre']=$row['nombre'];
+							$nombre=$_SESSION['nombre'];
+						
+							echo "Entrenador $nombre";
+						?>
+						<button><a href="procesos/procesarEntreno.php?id=1">CONTRATAR</a>
+					</div> 
                 </div>
                 
                 <div id="entreno_2">
+					<?php 
+                        $app = aplicacion::getSingleton();
+                        $conexion = $app->conexionBd();
+                        $tabla = "entrenamientos";
+                        if ($conexion->connect_error) {
+                            die("La conexion falló: " . $conexion->connect_error);
+                        }
+                        else{
+                            $query = "SELECT * FROM $tabla WHERE id = 21";
+                            $result = $conexion->query($query);
+                            $row = mysqli_fetch_assoc($result);
+                            $_SESSION['nombre']=$row['Nombre'];
+							$_SESSION['entrenador']=$row['Entrenador'];
+                        }
+					?>
                     <img src="https://uecluster.blob.core.windows.net/images/1506064164_shutterstock-314336384-b-jpg.jpg" width="500" height="330">
-                    <div id="texto-encima">Entrenamiento Definición </div>
-                    <div id="entrenador">Entrenador: Antonio Garcia Pinedo
-                        <button><a href="procesos/procesarEntreno.php?id=2">CONTRATAR</a></button>
-                    </div>
+                    <div id="texto-encima">
+						<?php
+							$nombre=$_SESSION['nombre'];
+							echo "$nombre";
+						?>
+					</div>
+					<div id="entrenador">
+						<?php
+							$entrenador=$_SESSION['entrenador'];
+							
+							$tabla = "entrenadores";
+							$query = "SELECT * FROM $tabla WHERE id = $entrenador";
+							$result = $conexion->query($query);
+							$row = mysqli_fetch_assoc($result);
+							$_SESSION['nombre']=$row['nombre'];
+							$nombre=$_SESSION['nombre'];
+						
+							echo "Entrenador $nombre";
+						?>
+						<button><a href="procesos/procesarEntreno.php?id=2">CONTRATAR</a>
+					</div> 
                 </div>
             </div>
         </div>
