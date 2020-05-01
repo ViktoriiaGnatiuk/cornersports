d3a3fca307273830f1eb9d0aec7eeca804186087
Vicky <Vicky>
2020-05-01 20:06:41 +0200

    Added: botones + y menos funcionales

--- productos.php
+++ productos.php
@@ -7,67 +7,67 @@ require_once __DIR__.'/includes/productos.php';
  <head>
 	<meta charset="utf-8">
 	<title>Productos</title>
-	<link rel="stylesheet" href="estilos/estiloDeportes.css?v=<?php echo(rand()); ?>" />
+	<link rel="stylesheet" href="estilos/estiloProductos.css?v=<?php echo(rand()); ?>" />
     <script src="/js/mi_script.js?v=<?php echo(rand()); ?>"></script>
  </head>
 
  <body>
     <div id="contenedor">
-	<?php
-		include __DIR__.'/includes/estructura/cabecera.php';
-	?>
-	<div id="contenido">
-		<div class="main">
-			<?php
-				$prd=new productos();
-				$i=0;
-				if(isset($_GET['tipo'])){
-					$tipo=$_GET['tipo'];
-					$_SESSION['tipo']=$tipo;
-				}else{
-					$tipo=$_SESSION['tipo'];
-				}
-					
-				$size=$prd->getSize($tipo);
-				$items=$prd->getItems($tipo);
+		<?php
+			include __DIR__.'/includes/estructura/cabecera.php';
+		?>
+		<div id="contenido">
+			<div class="main">
+				<?php
+					$prd=new productos();
+					$i=0;
+					if(isset($_GET['tipo'])){
+						$tipo=$_GET['tipo'];
+						$_SESSION['tipo']=$tipo;
+					}else{
+						$tipo=$_SESSION['tipo'];
+					}
+						
+					$size=$prd->getSize($tipo);
+					$items=$prd->getItems($tipo);
 
-				while( $i < $size){
-					$id = $items[$i]['id'];	
-					$nombre = $items[$i]['nombre'];
-					$imagen = $items[$i]['imagen'];
-					$descripcion = $items[$i]['descripcion'];
-					$precio = $items[$i]['precio'];
-					$precio_alquiler = $items[$i]['precio_alquiler'];
-					
-					$html = <<<EOF
-					<div class="producto">
-						<div class="izq">
-							<img class="imagen" src="$imagen">
-							<div class="nombre"><p>$nombre</p></div>
-						</div>
-						<div class="drc">
-							<p>Descripcion</p>
-							<div class="descripcion">
-								<p>$descripcion</p>
+					while( $i < $size){
+						$id = $items[$i]['id'];	
+						$nombre = $items[$i]['nombre'];
+						$imagen = $items[$i]['imagen'];
+						$descripcion = $items[$i]['descripcion'];
+						$precio = $items[$i]['precio'];
+						$precio_alquiler = $items[$i]['precio_alquiler'];
+						
+						$html = <<<EOF
+						<div class="producto">
+							<div class="izq">
+								<img class="imagen" src="$imagen">
+								<div class="nombre"><p>$nombre</p></div>
 							</div>
-							<p>Precio</p>
-							<div class="precio"><p>$precio €</p></div>
-							<form action="procesos/addItem.php?id=$id" id="form_session" method="post">
-							<input type="submit" class="comprar" value="Añadir">
-							</form>
+							<div class="drc">
+								<p>Descripcion</p>
+								<div class="descripcion">
+									<p>$descripcion</p>
+								</div>
+								<p>Precio</p>
+								<div class="precio"><p>$precio €</p></div>
+								<form action="procesos/addItem.php?id=$id" id="form_session" method="post">
+								<input type="submit" class="comprar" value="Añadir">
+								</form>
+							</div>
 						</div>
-					</div>
-					EOF;
-					echo"$html";
-					++$i;
-				}
-			?>
+						EOF;
+						echo"$html";
+						++$i;
+					}
+				?>
+			</div>
 		</div>
-	</div>
-	<?php	
-		include __DIR__.'/includes/estructura/barraOfertas.php';	
-		include __DIR__.'/includes/estructura/pie.php';
-	?>
+		<?php	
+			include __DIR__.'/includes/estructura/barraOfertas.php';
+			include __DIR__.'/includes/estructura/pie.php';
+		?>
 	</div>
  </body>
 </html>
