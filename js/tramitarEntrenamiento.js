d511e880bc32f22e338398f887217c4a7174ebcb
SergyC <63809662+SergyC@users.noreply.github.com>
2020-05-30 12:00:00 +0200

    Update tramitarEntrenamiento.js

--- js/tramitarEntrenamiento.js
+++ js/tramitarEntrenamiento.js
@@ -1,20 +1,83 @@
+var codigo_usado = false;
+var fecha_correcta = false;
+//Trunca los decimales de un número decimal
+truncateDecimals = function (number, digits) {
+    var multiplier = Math.pow(10, digits),
+        adjustedNum = number * multiplier,
+        truncatedNum = Math[adjustedNum < 0 ? 'ceil' : 'floor'](adjustedNum);
+
+    return truncatedNum / multiplier;
+};
+function fecha_actual(){
+    var d = new Date();
+
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+
+    var output = d.getFullYear() + '/' +
+    (month<10 ? '0' : '') + month + '/' +
+    (day<10 ? '0' : '') + day;
+    return output;
+}
 
 //Contrata un entrenamiento
 $(document).ready(function(){
-	
-    //Comprueba que todo este correcto y tramita el entrenamiento
-    function addEntrenamiento(data, status){
-        if(data==="no_logeado"){
-            location.href ="http://localhost/cornersports/login.php";
+
+	 $(".aplicar").click(function(){
+        var valor = $("#cod_desc").val();
+        if(valor == "UCM2605" && !codigo_usado){
+            var total = parseFloat(($("#total_real").text()));
+            var descuento = total*20/100;
+            var real = truncateDecimals((total - descuento),2);
+            $("#total_real").text(real);
+            var entrega = truncateDecimals(parseFloat($("#tr_t").text()) - total, 2);
+            $("#tr_t").text(real+entrega);
+            codigo_usado = true;
+            alert("Se ha aplicado un descuento de un 20% a su pedido.");
+        }else if (valor == "UCM2605" && codigo_usado){
+            alert("Este código ya ha sido usado.");
+        }else{
+            alert("El código no es valido.");
         }
-		else{
-			 alert("Se ha añadido el entrenamiento a su perfil");
-		}
+    });
+	
+	//Comprueba que la fecha de caducidad de la tarjeta de crédito sea correcta
+    $("#fecha_venc").change(function () {
+        var fecha_act = fecha_actual();
+        var fecha_caducidad = $(this).val();
+        if(new Date(fecha_caducidad).getTime() < new Date(fecha_act).getTime()){
+            alert("La fecha de caducidad de la tarjeta no es correcta.");
+        }else{
+            fecha_correcta=true;
+        }
+    });
+
+    //Comprueba que todo este correcto y tramita el pedido
+    function tramitado(data, status){
+        if(data==="correcto"){
+            alert("Tramitación correcta");
+        }else{
+            alert("No se ha podido tramitar el pedido");
+        }
     }
-  
-    $(".b_entrenamiento").click(function(){
-		var id = $(this).attr('id');
-		var url="http://localhost/cornersports/procesos/procesarEntreno.php?id=" + id;
-		$.get(url,addEntrenamiento);
-    });   
+	
+	function comprobarTarjeta(){
+        var mensaje = "Introduzca un número de tarjeta válido";
+        return cambiarFondo("#num_tarj", $("#num_tarj").val().length == 16 && !isNaN($("#num_tarj").val()), mensaje);
+    }
+    function comprobarTitular(){
+        var mensaje = "Introduzca el nombre del titular de la tarjeta";
+        return cambiarFondo("#titular_tr", $("#titular_tr").val() != "", mensaje);
+    }
+	
+	$(".b_entrenamiento").click(function(){
+        if(comprobarTarjeta() && comprobarTitular()){
+            if(fecha_correcta){
+				var url="../procesos/cambiarPrecioPedido.php?precio=" + parseFloat($("#tr_t").text());
+                $.get(url,tramitado);
+            }else{
+                alert("La fecha de caducidad de la tarjeta no es correcta.");
+            }
+        }     
+    });   	
 });
